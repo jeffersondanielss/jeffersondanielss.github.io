835740b0377cfd55fdc4b13d084527a3cf7c3d7c
Jefferson Daniel <jeffersondaniels@hotmail.com>
2016-07-02 17:59:43 -0300

    add projects

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,6 @@ module.exports = function( grunt ) {
   "use strict";
 
   grunt.initConfig({
-    uglify: {
-      options: {
-        mangle: true
-      },
-      my_target: {
-        files: {
-          'src/zipcode.min.js': ['zipcode.js']
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.registerTask('default', ['uglify']);
-}
-
-module.exports = function( grunt ) {
-
-  "use strict";
-
-  grunt.initConfig({
 
     uglify: {
       options: {
--- _site/assets/scripts/menu.js
+++ _site/assets/scripts/menu.js
@@ -2,21 +2,16 @@
 
 (function(){
   
-  var hamburger = document.getElementById('hamburger')
-    , menu = document.querySelector('.menu')
-    , menuItemBlog = document.querySelector('.menu__item--blog');
+  var hamburger = $('#hamburger')
+    , menu = $('.menu')
+    , menuItemBlog = $('.menu__item--blog');
 
   function toggleMenu() {
-    app.toggleClass(hamburger, 'open');
-    app.toggleClass(menu, 'menu--active');
+    hamburger.toggleClass('open');
+    menu.toggleClass('menu--active');
   }
 
-  hamburger.addEventListener('click', function() {
-    toggleMenu();
-  }, false);
-
-  menuItemBlog.addEventListener('click', function(e) {
-    toggleMenu();
-  }, false);
+  hamburger.click( toggleMenu );
+  menuItemBlog.click( toggleMenu );
 
 })();
